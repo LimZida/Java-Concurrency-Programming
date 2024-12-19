a86d2f537e0900f31739bcc14415f74d1bc767b5
LimZida <zida4470@naver.com>
2024-12-19 11:01:36 +0900

    20241219 context switching 연습

--- src/main/java/io/concurrency/chapter01/exam02/ContextSwitchExample.java
+++ src/main/java/io/concurrency/chapter01/exam02/ContextSwitchExample.java
@@ -1,5 +1,11 @@
 package io.concurrency.chapter01.exam02;
-
+/*
+* 컨텍스트 스위칭
+*
+* 프로세스 혹은 스레드 간의 작업 도중에 I/O 혹은 오버헤드가 발생하는 경우, 다른 작업과의 변환을 진행한다.
+*
+* 해당 과정에서 기존 PCB 혹은 TCB 정보를 삭제하고 캐시를 비운 후, 새로운 PCB 혹은 TCB 정보로 변경된다.
+* */
 public class ContextSwitchExample {
     public static void main(String[] args) {
 
